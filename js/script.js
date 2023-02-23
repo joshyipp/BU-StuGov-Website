8eec81565d286045e5da7d08126b46c10bbb6e1c
joshyipp <joshyip@bu.edu>
2023-02-22 23:06:39 -0500

    upodate

--- js/script.js
+++ js/script.js
@@ -109,3 +109,13 @@ for (let i = 0; i < Class('navbtn').length; i++){
     nav.classList.toggle('active')
     }
 }
+
+var userFeed = new Instafeed({
+    get: 'user',
+    target: "instafeed-container",
+    resolution: 'low_resolution',
+    template:	'<div class="content_img"><div><a href="{{link}}"><img title="{{caption}}" src="{{image}}" /></a><p>{{caption}}</p> </div></div>'
+    /*'<a href="{{link}}"><img title="{{caption}}" src="{{image}}" /></a>'*/,
+    accessToken: 'IGQVJWci1McEdaTEMtWjlib0xQTW02YUtXSFlPM0RrQWZASS3RYRnVISlFZAcFZAXWGdydnhlanplM2EtMmxhckNxVW5KREkwZATBDLWJleDZAKUy1MamhfM1o0RFF3X183dFItMjc0ME4zNEU3SjZAoV0x5ZAAZDZD',
+});
+userFeed.run();
